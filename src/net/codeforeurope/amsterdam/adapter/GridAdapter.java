98094ca04ed167624959b09a2fd36b3713b55753
Piotr Steininger <piotr.steininger@gmail.com>
2013-06-03 23:42:27 +0200

    moved grid adapter

--- src/net/codeforeurope/amsterdam/adapter/GridAdapter.java
+++ src/net/codeforeurope/amsterdam/adapter/GridAdapter.java
@@ -1,18 +1,21 @@
-package net.codeforeurope.amsterdam;
+package net.codeforeurope.amsterdam.adapter;
 
 import java.util.List;
 
+import net.codeforeurope.amsterdam.R;
 import net.codeforeurope.amsterdam.model.BaseModelWithIcon;
 import net.codeforeurope.amsterdam.util.StreamDrawable;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GridAdapter extends BaseAdapter {
 
@@ -36,34 +39,43 @@ public class GridAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		final MainListHolder mHolder;
 		GridView gv = (GridView) parent;
+
 		View v = convertView;
+
 		if (convertView == null) {
 			mHolder = new MainListHolder();
-			v = inflater.inflate(R.layout.profile_item, null);
-			mHolder.icon = (ImageView) v.findViewById(R.id.gridIcon);
+			v = inflater.inflate(R.layout.grid_item, null);
+			mHolder.topText = (TextView) v.findViewById(R.id.grid_item_top);
+			mHolder.bottomText = (TextView) v.findViewById(R.id.grid_item_bottom);
 			v.setTag(mHolder);
 		} else {
 			mHolder = (MainListHolder) v.getTag();
-		}
 
+		}
 		final BaseModelWithIcon model = profiles.get(position);
-		Bitmap bitmap = BitmapFactory.decodeFile(model.image.localPath);
-//		BitmapDrawable bmd = new BitmapDrawable(context.getResources(),
-//				model.image.localPath);
-//		bmd
-//		mHolder.icon.setBackgroundDrawable();
-		
-		StreamDrawable drawable = new StreamDrawable(bitmap, 6, 4);
-		mHolder.icon.setImageDrawable(drawable);
-		// v.setLayoutParams(new GridView.LayoutParams(130, 130));
-		// mHolder.icon.setScaleType(ImageView.ScaleType.CENTER_CROP);
+		LayerDrawable layerDrawable = getBackground(model);
+		v.setBackgroundDrawable(layerDrawable);
+		mHolder.topText.setText(model.nameEn);
+		mHolder.bottomText.setText("6 routes");
 		return v;
 
 	}
 
+	private LayerDrawable getBackground(final BaseModelWithIcon model) {
+		Bitmap bitmap = BitmapFactory.decodeFile(model.image.localPath);
+		StreamDrawable drawable = new StreamDrawable(bitmap, 6, 2);
+		Drawable drawable2 = context.getResources().getDrawable(
+				R.drawable.grid_item);
+
+		LayerDrawable layerDrawable = new LayerDrawable(new Drawable[] {
+				drawable, drawable2 });
+		return layerDrawable;
+	}
+
 	class MainListHolder {
 
-		private ImageView icon;
+		private TextView topText;
+		private TextView bottomText;
 
 	}
 
