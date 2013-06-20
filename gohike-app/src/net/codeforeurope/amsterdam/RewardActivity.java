13ae7b045c17143d54211f8de9059bf5eb3b6681
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-06-20 19:34:28 +0200

    Added Facebook SDK for picture upload

--- gohike-app/src/net/codeforeurope/amsterdam/RewardActivity.java
+++ gohike-app/src/net/codeforeurope/amsterdam/RewardActivity.java
@@ -1,11 +1,13 @@
 package net.codeforeurope.amsterdam;
 
+
 import net.codeforeurope.amsterdam.model.GameData;
 import net.codeforeurope.amsterdam.model.Profile;
 import net.codeforeurope.amsterdam.model.Reward;
 import net.codeforeurope.amsterdam.model.Route;
 import net.codeforeurope.amsterdam.util.ApiConstants;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
@@ -17,6 +19,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.*;
 
 public class RewardActivity extends AbstractGameActivity {
 
@@ -33,6 +37,7 @@ public class RewardActivity extends AbstractGameActivity {
 	TextView rewardTitle;
 
 	TextView rewardDescription;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -73,14 +78,18 @@ public class RewardActivity extends AbstractGameActivity {
 			return true;
 		case R.id.menu_share_badge:
 			// share on Facebook
+			
+			
+            onClickPostPhoto();
 
-			Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-			Uri screenshotUri = Uri.parse(reward.image.localPath);
-			// Bitmap photo = BitmapFactory.decodeFile(reward.image.localPath);
-			sharingIntent.setType("image/png");
-			sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
-			startActivity(Intent.createChooser(sharingIntent,
-					getString(R.string.share_reward_using)));
+			//Following code is to share using ACTION_SEND
+//			Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+//			Uri screenshotUri = Uri.parse(reward.image.localPath);
+//			// Bitmap photo = BitmapFactory.decodeFile(reward.image.localPath);
+//			sharingIntent.setType("image/png");
+//			sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+//			startActivity(Intent.createChooser(sharingIntent,
+//					getString(R.string.share_reward_using)));
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -125,4 +134,40 @@ public class RewardActivity extends AbstractGameActivity {
 		// TODO Auto-generated method stub
 
 	}
+	
+    private void onClickPostPhoto() {
+    	 Bitmap image = BitmapFactory.decodeFile(reward.image.localPath);
+         Request request = Request.newUploadPhotoRequest(Session.getActiveSession(), image, new Request.Callback() {
+             @Override
+             public void onCompleted(Response response) {
+                 showPublishResult(reward.descriptionNl, response.getGraphObject(), response.getError());
+             }
+         });
+         request.executeAsync();
+    }
+	
+    private interface GraphObjectWithId extends GraphObject {
+        String getId();
+    }
+    
+    private void showPublishResult(String message, GraphObject result, FacebookRequestError error) {
+        String title = null;
+        String alertMessage = null;
+        if (error == null) {
+            title = "Success!";
+            String id = result.cast(GraphObjectWithId.class).getId();
+            alertMessage = "Picture posted OK"; //getString(R.string.successfully_posted_post, message, id);
+        } else {
+            title = "Error during post";
+            alertMessage = error.getErrorMessage();
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(alertMessage)
+                .setPositiveButton("OK", null)
+                .show();
+    }
+    
 }
+
